06a171b267fd4782f1439fc19a0fe709dd46a780
Alayande-442 <alayandesteven@gmail.com>
2025-08-04 13:29:38 +0100

    create EducationForm UI

--- src/app/(main)/editor/forms/EducationForm.tsx
+++ src/app/(main)/editor/forms/EducationForm.tsx
@@ -1,8 +1,36 @@
 import { EditorFormProps } from "@/lib/types";
+import { educationSchema, educationValues } from "@/lib/validation";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useEffect } from "react";
+import { useFieldArray, useForm } from "react-hook-form";
 
 export default function EducationForm({
   resumeData,
   setResumeData,
 }: EditorFormProps) {
+  const form = useForm<educationValues>({
+    resolver: zodResolver(educationSchema),
+    defaultValues: {
+      educations: resumeData.educations || [],
+    },
+  });
+
+  // COMMENT use Effect to handle image upload
+  useEffect(() => {
+    const { unsubscribe } = form.watch(async (values) => {
+      const isValid = await form.trigger();
+      if (!isValid) return;
+      setResumeData({
+        ...resumeData,
+        educations: values.educations?.filter((edu) => edu !== undefined) || [],
+      });
+    });
+    return unsubscribe;
+  }, [form, resumeData, setResumeData]);
+
+  const { fields, append, remove } = useFieldArray({
+    control: form.control,
+    name: "educations",
+  });
   return <div>education</div>;
 }
